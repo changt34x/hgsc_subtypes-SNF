c0a0a4d784a45a9ee8dfe62a4d25d3c6a4f6ca50
changt34x <changt34x@gmail.com>
2016-09-23 03:03:51 -0400

    Analysis script

---
+++ ANALYSIS.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+exec &>hgsc_analysis.out 
+
+############################################
+# Similarity Network Fusion analysis of HGSC datasets for subtyping
+# 
+# Chang, T
+# ~~~~~~~~~~~~~
+# This script stores instructions to perform subtyping of HGSC samples. 
+# All scripts and relevant files are included in the repository and the 
+# workflow depends on the running sequential scripts within  the larger
+# folder structure. See the README for general information and INSTALL.R
+# for package dependencies.
+# ~~~~~~~~~~~~~~~~~~~~~
+############################################
+
+################
+# Part 0:
+# Constants
+################
+CLUSTERMIN=2
+CLUSTERMAX=4
+DATA1="TCGA_HumanMethylation27
+DATA2="TCGA_miRNA_HiSeq
+K=20
+SIGMA=0.5
+T=20
+ITERMAX=20
+STARTS=20
+
+
+################
+# Part 1:
+# SNF Grouping
+################
+R --no-save --args $CLUSTERMIN $CLUSTERMAX $K $SIGMA $T $DATA1 $DATA2 \
+< script/snfGrouping.R
+
+################
+# Part 2:
+# Fit Testing
+################
+R --no-save --args $CLUSTERMIN $CLUSTERMAX $ITERMAX $STARTS $DATA1 $DATA2 \
+< script/fit.R
